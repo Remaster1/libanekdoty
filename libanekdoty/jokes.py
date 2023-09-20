a1fc2ee3a07939afabd37d94274feae9e5f63474
Remaster <degtyareviv080@gmail.com>
2023-09-20 19:36:06 +0200

    True parse all pages

--- libanekdoty/jokes.py
+++ libanekdoty/jokes.py
@@ -20,13 +20,26 @@ class Jokes:
     def get_all_jokes(self) -> list[dict[str, Any]]:
         """Get all jokes from category"""
         jokes = []
-        request = requests.get(self.URL)
-        if request.status_code == 404:
-            raise LibanekdotyException("Server returned 404")
-        parser = BeautifulSoup(request.text, self.parser_type)
-        joke_list = parser.find("ul", {"class": "item-list"}).find_all("li", recursive=False)
-        for joke in joke_list:
-            joke_text = joke.find("div", {"class": "holder-body"}).p.text
-            top = joke.find("div", {"class": "like-counter"}).text
-            jokes.append({"text": joke_text, "top": top})
+        current_url = self.URL
+
+        while True:
+            request = requests.get(current_url)
+
+            if request.status_code == 404:
+                raise LibanekdotyException("Server returned 404")
+
+            parser = BeautifulSoup(request.text, self.parser_type)
+
+            joke_list_element = parser.find("ul", {"class": "item-list"}).find_all("li", recursive=False)
+            for joke in joke_list_element:
+                joke_text = joke.find("div", {"class": "holder-body"}).p.text
+                top = joke.find("div", {"class": "like-counter"}).text
+                jokes.append({"text": joke_text, "top": top})
+
+            next_page_element = parser.find("li", {"class": "next"})
+            next_page_link = next_page_element.find("a")
+
+            if next_page_link is None:  # checks that the link to the next page is not None
+                break
+            current_url = next_page_link["href"]
         return jokes
